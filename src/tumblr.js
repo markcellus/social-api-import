e467d38ffc125775bd5fccc8f42f15f969cb71bd
Mark Kennedy <mark.kennedy@akqa.com>
2015-02-07 22:31:49 -0500

    Adding Tumblr class

---
+++ src/tumblr.js
@@ -0,0 +1,84 @@
+define(['./utils', './base-api'], function (Utils, BaseApi) {
+    'use strict';
+
+    /**
+     * Tumblr API-loading class.
+     * @class Tumblr
+     */
+    var Tumblr = Utils.extend({}, BaseApi, {
+
+        /**
+         * Loads the script to the API.
+         * @param {Object} options - load options
+         * @param {Object} options.apiConfig - The Tumblr api options
+         * @param {Object} options.apiConfig.base-hostname - The base-hostname
+         * @param {Object} options.apiConfig.api_key - API key
+         * @param {Function} [callback] - Fires when the FB SDK has been loaded
+         */
+        load: function (options, callback) {
+            var onReadyCallback = 'onTumblrReady';
+
+            this.options = options = Utils.extend({
+                apiConfig: {}
+            }, options);
+
+            window[onReadyCallback] = function () {
+                callback ? callback() : null;
+            };
+
+            if (!options.apiConfig['base-hostname']) {
+                return console.error('Tumblr load() method needs to be passed a "base-hostname"');
+            }
+
+            options.apiConfig.api_key = options.apiConfig.api_key || '';
+
+            options.scriptUrl = '//api.tumblr.com/v2/blog/' + options.apiConfig['base-hostname'] + '/?' +
+                'api_key=' + options.apiConfig.api_key + '&' +
+                'callback=' + onReadyCallback;
+
+            this.loadScript(document, options.scriptUrl, 'tumblr-lscript');
+        },
+
+        /**
+         * Makes an API call for latest posts and returns them in a standard block of html.
+         * @param url
+         * @param options
+         * @param callback
+         */
+        getPostsEmbed: function (url, options, callback) {
+
+            var request = new XMLHttpRequest(),
+                err,
+                onComplete = function (err) {
+                    callback ? callback(err) : null;
+                };
+
+            if (typeof options === 'function') {
+                options = callback;
+            }
+
+            // add api key to call
+            url += '?api_key=' + this.options.apiConfig.api_key + '&jsonp=';
+
+            request.open('GET', url, true);
+            request.onload = function() {
+                if (request.status >= 200 && request.status < 400) {
+                    // Success!
+                    var data = JSON.parse(request.responseText);
+                } else {
+                    // error
+                    err = request
+                }
+                onComplete(err);
+            };
+            request.onerror = function () {
+                console.log('error occurred');
+                console.log(arguments);
+            };
+            request.send();
+        }
+
+    });
+
+    return Tumblr;
+});
