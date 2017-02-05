374ce05612138015e188e0663cd5dfdb83eb2622
Mark <mark@heyimmark.com>
2017-02-05 04:19:24 -0500

    Upgrade build tools

--- bt-config.js
+++ bt-config.js
@@ -1,22 +1,19 @@
-'use strict';
 module.exports = {
     build: {
-        production: {
-            files: {
-                'dist/social-api.js': ['index.js']
-            },
-            browserifyOptions: {
-                standalone: 'SocialAPI'
-            },
-            minifyFiles: {
-                'dist/social-api-min.js': ['dist/social-api.js']
-            },
-            bannerFiles: ['dist/*']
-        }
+        files: {
+            'dist/social-api.js': ['index.js']
+        },
+        browserifyOptions: {
+            standalone: 'SocialAPI'
+        },
+        minifyFiles: {
+            'dist/social-api-min.js': ['dist/social-api.js']
+        },
+        bannerFiles: ['dist/*']
     },
     tests: {
         mocha: {
-            src: ['tests/*.js']
+            files: ['tests/*.js']
         }
     }
 };
