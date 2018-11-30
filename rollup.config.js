7af75edf251dda09e2b1747d4cffddb52c7ae003
Mark <mark@heyimmark.com>
2018-11-30 16:17:31 -0500

    Only watch files when explicitly set in run commands

--- rollup.config.js
+++ rollup.config.js
@@ -2,31 +2,17 @@ import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
-const filenames = [
-    'facebook',
-    'instagram',
-    'tumblr',
-    'twitter',
-    'vine',
-    'index'
-];
+const filenames = ['facebook', 'instagram', 'tumblr', 'twitter', 'vine', 'index'];
 
-const exports = filenames.map((name) => {
-   return {
-       input: `src/${name}.ts`,
-       output: {
-           format: 'esm',
-           file: `dist/${name}.js`
-       },
-       plugins: [
-           resolve(),
-           typescript(),
-           commonjs()
-       ],
-       watch: {
-           include: 'src/**'
-       }
-   }
+const exports = filenames.map(name => {
+    return {
+        input: `src/${name}.ts`,
+        output: {
+            format: 'esm',
+            file: `dist/${name}.js`
+        },
+        plugins: [resolve(), typescript(), commonjs()]
+    };
 });
 
-export default exports
+export default exports;
